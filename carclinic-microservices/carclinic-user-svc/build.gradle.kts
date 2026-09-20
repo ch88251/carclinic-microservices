1128ab93d3780720ba026f4ae23d25ea88f0160e
Charles Hayes <cfhayes16@gmail.com>
2026-09-20 17:04:24 -0600

    test: Generate Jacoco report

--- carclinic-microservices/carclinic-user-svc/build.gradle.kts
+++ carclinic-microservices/carclinic-user-svc/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	java
+	jacoco
 	id("org.springframework.boot") version "4.1.1"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -40,4 +41,8 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
 }
